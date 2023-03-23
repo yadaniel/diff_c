6da6b606757c95a57ca55d4922e1601af90bcf44
yadaniel <ya.daniel@inbox.ru>
2023-03-23 13:22:10 +0100

    checksum lists filenames

--- _mk_clang_checksum.sh
+++ _mk_clang_checksum.sh
@@ -11,14 +11,14 @@ for i in $(find . -maxdepth 1 -iname "*.c"); do
     echo $i
 
     cp ${i} ${tmp}.c
-    ${CC} -c ${tmp}.c -o ${tmp}.o
-    md5sum.exe ${tmp}.o >> _md5
-    rm -f ${tmp}.o
+    ${CC} -c ${tmp}.c -o ${i}.o
+    md5sum.exe ${i}.o >> _md5
+    rm -f ${i}.o
 
     cp ${i}_ ${tmp}.c || (echo "no formatted cfiles found" && exit 1)
-    ${CC} -c ${tmp}.c -o ${tmp}.o
-    md5sum.exe ${tmp}.o >> _md5
-    rm -f ${tmp}.o
+    ${CC} -c ${tmp}.c -o ${i}_.o
+    md5sum.exe ${i}_.o >> _md5
+    rm -f ${i}_.o
 
     echo "" >> _md5
     echo
--- _rm_clang.sh
+++ _rm_clang.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
 
-find . -regextype awk -iregex ".*\.[ch]_" -exec rm {} \;
+#find . -regextype awk -iregex ".*\.[ch]_" -exec rm {} \;
+
+rm -f ./current/*.c_ ./current/*.h_ ./current/_md5
+rm -f ./prev/*.c_ ./prev/*.h_ ./prev/_md5
 
